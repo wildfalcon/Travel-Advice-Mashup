00c671e5a584c9f5e4cc5fafd1a7a5a8337e08ea
Wildfalcon <laurie@wildfalcon.com>
2011-04-05 11:04:45 +0100

    Initial import of rails app

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'mysql2'
+
+group :development, :test do
+	gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelAlertMashup::Application.config.secret_token = '6d18525712d5c60a2e8c11aae4c7202a8ccc1ce995dbf9e3c9ce772b834bee72135c2fd596ff13fea7a47aed1cd61e580c493bf80b943b024ffd17393a1dec88'
